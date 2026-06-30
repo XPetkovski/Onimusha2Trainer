f798e203499fd44f848b172fc61b58b1074a7c3d
hristijanpetkovski <skifaller@gmail.com>
2026-06-30 21:37:52 +0200

    new build

--- obj/Release/net10.0/win-x64/Onimusha2Trainer.AssemblyInfo.cs
+++ obj/Release/net10.0/win-x64/Onimusha2Trainer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Onimusha2Trainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5661209495d96f676c3b8d42116670ef6706d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Onimusha2Trainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Onimusha2Trainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
